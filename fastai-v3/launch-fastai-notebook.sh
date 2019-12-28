499c5f02cd1415afa0b16b801f437d73020bb63f
Ran Tao <rantaoca@google.com>
2019-12-28 04:03:52 -0800

    Fix bugs, notebook works on GPU.

--- fastai-v3/launch-fastai-notebook.sh
+++ fastai-v3/launch-fastai-notebook.sh
@@ -4,7 +4,24 @@ is_exist=$?
 
 if [[ $is_exist == 1 ]]; then
   # Does not exist, create it.
-  docker run -it -u $(id -u):$(id -g) -v $(realpath ~/playground/fastai/course-v3):/notebooks/course-v3 -p 8888:8888 --name fastai-instance fastai
+
+  # The following parameters:
+  # --gpus all
+  #    Use all GPUs
+  # -it
+  #    Run docker interactively.
+  # -u $(id -u):$(id -g)
+  #    Run docker with the current user, so we can modify mounted directory.
+  # --ipc=host
+  #    Set interprocess communication to use the host's IPC namespace. This is
+  #    to fix a bug where the DataLoader with multiple workers.
+  # -v host_path:docker_path
+  #    Mount the directory.
+  # -p 8888:8888
+  #    Port forward the notebook.
+  # --name fastai-instance
+  #    Name the container.
+  docker run --gpus all -it -u $(id -u):$(id -g) --ipc=host -v $(realpath ~/playground/fastai/course-v3):/notebooks/course-v3 -p 8888:8888 --name fastai-instance fastai
 elif [[ $state_running != "true" ]]; then
   # Exists but it's stopped
   docker start -i fastai-instance
