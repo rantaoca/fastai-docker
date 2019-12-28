568cc3dd7f9f212184e2982507cf61e656f6761f
Ran Tao <rantaoca@google.com>
2019-12-28 02:24:23 -0800

    Upgrade launch script to support starting a server after it's been stopped, instead of deleting server upon stop

--- fastai-v3/launch-fastai-notebook.sh
+++ fastai-v3/launch-fastai-notebook.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-docker run -it --rm -u $(id -u):$(id -g) -v $(realpath ~/playground/fastai/course-v3):/notebooks/course-v3 -p 8888:8888 fastai
+state_running=`docker inspect -f '{{.State.Running}}' fastai-instance`
+is_exist=$?
+
+if [[ $is_exist == 1 ]]; then
+  # Does not exist, create it.
+  docker run -it -u $(id -u):$(id -g) -v $(realpath ~/playground/fastai/course-v3):/notebooks/course-v3 -p 8888:8888 --name fastai-instance fastai
+elif [[ $state_running != "true" ]]; then
+  # Exists but it's stopped
+  docker start -i fastai-instance
+else
+  echo "fast.ai notebook is already running."
+fi
 
