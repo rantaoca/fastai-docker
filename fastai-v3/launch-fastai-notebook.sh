e11e4879d44bbe4807bb81f120de93b1933cc761
Ran Tao <rantaoca@google.com>
2019-12-28 05:23:34 -0800

    Fix cuda version

--- fastai-v3/launch-fastai-notebook.sh
+++ fastai-v3/launch-fastai-notebook.sh
@@ -12,16 +12,22 @@ if [[ $is_exist == 1 ]]; then
   #    Run docker interactively.
   # -u $(id -u):$(id -g)
   #    Run docker with the current user, so we can modify mounted directory.
-  # --ipc=host
-  #    Set interprocess communication to use the host's IPC namespace. This is
-  #    to fix a bug where the DataLoader with multiple workers.
+  # --shm-size 16G
+  #    Give docker 16gb shared memory for IPC. This is to fix a bug where the
+  #    DataLoader with multiple workers.
   # -v host_path:docker_path
   #    Mount the directory.
   # -p 8888:8888
   #    Port forward the notebook.
   # --name fastai-instance
   #    Name the container.
-  docker run --gpus all -it -u $(id -u):$(id -g) --ipc=host -v $(realpath ~/playground/fastai/course-v3):/notebooks/course-v3 -p 8888:8888 --name fastai-instance fastai
+  docker run --gpus all \
+    -it -u $(id -u):$(id -g) \
+    --shm-size 16G \
+    -v $(realpath ~/playground/fastai/course-v3):/notebooks/course-v3 \
+    -p 8888:8888 \
+    --name fastai-instance \
+    fastai
 elif [[ $state_running != "true" ]]; then
   # Exists but it's stopped
   docker start -i fastai-instance
